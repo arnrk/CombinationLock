38364d1eb9a98fc432e4c793e682ceb94fe9ae4e
Aaron Rock <aaronrk@mail.com>
2019-11-20 20:33:58 -0500

    Set Up Game Loop

---
+++ CombinationGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CombinationGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Combination Game");
+
+            bool inAction = true;
+            int attempts = 1;
+
+            Random Random = new Random();
+            int rand = Random.Next(1, 6);
+            int randOne = Random.Next(1, 6);
+            int randTwo = Random.Next(1, 6);
+            int randThree = Random.Next(1, 6);
+            Console.WriteLine($"Random 4 digit answer is {rand}{randOne}{randTwo}{randThree}");
+
+
+            while (inAction)
+            {
+                while (attempts < 11)
+                {
+                    Console.WriteLine("This Is Attempt Number: {0}", attempts);
+                    Console.WriteLine("What Is Your Guess");
+                    attempts++;
+                }
+
+                if (attempts > 11)
+                    Console.WriteLine("You Lose");
+                //break;
+            }
+            
+        }
+    
+    }
+}
